75229ef35e97d00b99b6ea2d2b5f0f857c44a87d
Evan <ehutch7@gmail.com>
2021-01-18 11:42:38 -0500

    Adding some more REST actions.

--- first/app.py
+++ first/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "evan"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda item: item["name"] == name, items), None)
         if not item:
@@ -23,6 +30,25 @@ class Item(Resource):
         item = {"name": name, "price": request_data["price"]}
         items.append(item)
         return (item, 201)
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x["name"] != name, items))
+        return {"message": "Item deleted"}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "price", type=float, required=True, help="this field cannot be left blank!"
+        )
+        data = parser.parse_args()
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data["price"]}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
